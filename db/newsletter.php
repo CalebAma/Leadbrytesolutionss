f76f5bd007c224092e5c918903a2fe0330670ea1
Caleb Amartey Amarh <132031642+CalebAma@users.noreply.github.com>
2025-11-11 17:20:10 +0000

    Updated files

--- db/newsletter.php
+++ db/newsletter.php
@@ -16,6 +16,36 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $stmt = $conn->prepare("INSERT INTO newsletter_subscribers (email) VALUES (?)");
         $stmt->execute([$email]);
         
+        // Send welcome email
+        require_once __DIR__ . '/PHPMailer/PHPMailer.php';
+        require_once __DIR__ . '/PHPMailer/SMTP.php';
+        require_once __DIR__ . '/PHPMailer/Exception.php';
+        
+        use PHPMailer\PHPMailer\PHPMailer;
+        use PHPMailer\PHPMailer\Exception;
+        
+        $mail = new PHPMailer(true);
+        try {
+            //Server settings
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'YOUR_GMAIL_ADDRESS@gmail.com'; // TODO: set your email
+            $mail->Password = 'YOUR_APP_PASSWORD'; // TODO: set your app password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            $mail->Port = 587;
+            //Recipients
+            $mail->setFrom('YOUR_GMAIL_ADDRESS@gmail.com', 'LeadBryte Solutions');
+            $mail->addAddress($email);
+            // Content
+            $mail->isHTML(true);
+            $mail->Subject = 'Welcome to LeadBryte Solutions Newsletter!';
+            $mail->Body    = '<h2>Thank you for subscribing!</h2><p>You are now subscribed to our newsletter.</p>';
+            $mail->AltBody = 'Thank you for subscribing! You are now subscribed to our newsletter.';
+            $mail->send();
+        } catch (Exception $e) {
+            // Email failed, but don't block subscription
+        }
         echo json_encode([
             'success' => true,
             'message' => 'Successfully subscribed to newsletter!'
